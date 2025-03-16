86047acf3f9d8708cd2892c779eccb5ddcf1d4bf
mhttekin <cb221086@qmul.ac.uk>
2025-03-16 22:01:38 +0000

    comma

--- back/backend/settings.py
+++ back/backend/settings.py
@@ -52,7 +52,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
