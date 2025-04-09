568d1161bc30b8f7bcd0486d094f7b45fba18690
mhttekin <cb221086@qmul.ac.uk>
2025-04-09 23:57:58 +0100

    history

--- front/src/app/transactionhistory/page.js
+++ front/src/app/transactionhistory/page.js
@@ -24,6 +24,18 @@ export default function TransactionHistory() {
     withdrawal: "Withdrawn",
   };
 
+  const formatNumber = (value, mode) => {
+    const number = Number(value);
+    if (isNaN(number)) return "0.00";
+
+    return number.toLocaleString(undefined, {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: mode ? 2 : 7,
+    });
+  };
+
+
+
   useEffect(() => {
     if (!user && !loading){
       router.push('/login');
@@ -62,15 +74,32 @@ export default function TransactionHistory() {
         {transactions.length === 0 ? (
           <p className="flex w-full h-full justify-center self-center">No transactions found.</p>
         ) : (
-          <div className="flex flex-col justify-center gap-5 w-full mt-15">
+          <div className="flex flex-col justify-center gap-3 w-full mt-15">
             {transactions.map((txn) => (
-              <div className="flex flex-row w-full gap-3 items-center" key={txn.id}>
-                <div className="w-7 h-7 flex items-center">
+              <div className="flex flex-row h-15 w-full gap-3 items-center" key={txn.id}>
+                <div className="w-7 h-full flex">
                   <img src={`/assets/${fileMap[txn.coin_symbol]}.png`} className="w-7 h-7"/>
                 </div>
-                <div className="w-20 h-7 flex items-center">
-                  <h1 className="font-bold text-sm">{messageMap[txn.transaction_type]}</h1> 
-                  <h1></h1>
+                <div className="w-40 flex-2 h-full flex flex-col items-start">
+                  <h1 className="font-bold text-sm">
+                    {txn.transaction_type === "transfer"
+                      ? wallets.some(wallet => wallet.public_key === txn.wallet_public_key)
+                        ? "Sent"
+                        : "Received"
+                      : messageMap[txn.transaction_type]}
+                  </h1> 
+                  {wallets && txn.transaction_type === 'transfer' && (
+                    <h1 className="text-left text-gray-500 text-sm">  {wallets.some(wallet => wallet.public_key === txn.wallet_public_key)
+                    ? `To: ${formatAddress(txn.counterparty_public_key)}`
+                    : `From: ${formatAddress(txn.wallet_public_key)}`}
+                    </h1>
+                  )}
+                </div>
+                <div className="flex flex-1 h-full flex-col items-end">
+                  <h1 className="text-sm">${formatNumber(txn.amount * marketData[txn.coin_symbol].price, true)}</h1>
+                  <h1 className="text-xs text-gray-500">{formatNumber(txn.amount, false)}
+                    {'\u00A0'}{txn.coin_symbol}
+                  </h1>
                 </div>
               </div>
             ))}
