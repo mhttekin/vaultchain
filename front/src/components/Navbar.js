858958abdb20479df5f3a475079bda2dd459623c
ytalhakivrak <yusuf.t.kivrak@gmail.com>
2025-04-10 05:15:01 +0100

    LAST FINALE

--- front/src/components/Navbar.js
+++ front/src/components/Navbar.js
@@ -21,15 +21,12 @@ const Navbar = () => {
         <Link href="/" className="nav-link">
           <Home className={`w-6 h-6 ${pathName === "/" ? 'drop-shadow-[0_0_12px_oklch(80%_0.1_262)] fill-blue-600' : 'fill-white'}`}/>
         </Link>
-        <Link href="/transactionhistory" className="nav-link">
-          <History className={`w-6 h-6 ${pathName === "/transactionhistory" ? 'drop-shadow-[0_0_12px_oklch(100%_0.1_262)] stroke-blue-600' : 'stroke-white'}`}/>
-        </Link>
         <Link href="/send" className="nav-link">
           <Transfer className={`w-6 h-6  
             ${pathName === "/send" ? 'drop-shadow-[0_0_12px_oklch(100%_0.1_262)]  stroke-blue-600' : 'stroke-white'}`}/> 
         </Link>
-        <Link href="/wallet" className="nav-link">
-          <Wallet className={`w-6 h-6 ${pathName === "/wallet" ? 'drop-shadow-[0_0_12px_oklch(80%_0.1_262)] fill-blue-600' : 'fill-white'}`}/>
+        <Link href="/transactionhistory" className="nav-link">
+          <History className={`w-6 h-6 ${pathName === "/transactionhistory" ? 'drop-shadow-[0_0_12px_oklch(100%_0.1_262)] stroke-blue-600' : 'stroke-white'}`}/>
         </Link>
       </nav>
   );
